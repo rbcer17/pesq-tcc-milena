ec606a7c9fb1b98443f5d25d143ac9b68c8b3025
Roberto Cavalcanti <63593378+rbcer17@users.noreply.github.com>
2022-11-18 13:24:15 -0300

    Create anova-area-queimada.R

---
+++ scripts/anova-area-queimada.R
@@ -0,0 +1,2 @@
+# Analise de variancia 2 way entre parques e entre periodos (el nino, la nina, neutro) com variavel dependente percentagem ou proporcao queimada
+# por ano. Transformacao arcoseno para normalizar dados proporcao
