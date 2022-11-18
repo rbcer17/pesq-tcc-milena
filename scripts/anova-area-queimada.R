3a8d9b160ad1e1d648c1b33b568b73eca443e412
Roberto Cavalcanti <rbcer10@gmail.com>
2022-11-18 13:55:18 -0300

    primeiros edits para o script

--- scripts/anova-area-queimada.R
+++ scripts/anova-area-queimada.R
@@ -1,2 +1,8 @@
 # Analise de variancia 2 way entre parques e entre periodos (el nino, la nina, neutro) com variavel dependente percentagem ou proporcao queimada
 # por ano. Transformacao arcoseno para normalizar dados proporcao
+library(readxl)
+anova_area_queimada_parque_enso <- read_excel("dados/anova-area-queimada-parque-enso.xlsx", 
+                                              col_types = c("text", "numeric", "text", 
+                                                            "text", "numeric", "numeric", "numeric"))
+View(anova_area_queimada_parque_enso)
+parquesqueimada<- anova_area_queimada_parque_enso
