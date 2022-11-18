32dd7837f92d42f570308f0bdfdce0b9cb5eb0c6
Roberto Cavalcanti <rbcer10@gmail.com>
2022-11-18 14:08:32 -0300

    comandos para credenciais

--- scripts/anova-area-queimada.R
+++ scripts/anova-area-queimada.R
@@ -1,5 +1,9 @@
 # Analise de variancia 2 way entre parques e entre periodos (el nino, la nina, neutro) com variavel dependente percentagem ou proporcao queimada
 # por ano. Transformacao arcoseno para normalizar dados proporcao
+#
+usethis::use_git_config(user.name = "Roberto Cavalcanti", user.email = "rbcer10@gmail.com")
+credentials::set_github_pat("YOURPATCODEHERE")
+#
 library(readxl)
 anova_area_queimada_parque_enso <- read_excel("dados/anova-area-queimada-parque-enso.xlsx", 
                                               col_types = c("text", "numeric", "text", 
