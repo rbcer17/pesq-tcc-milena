5c7cc2373e45bc3c597403e8eec8c0556acb542e
Roberto Cavalcanti <rbcer10@gmail.com>
2022-11-18 14:43:37 -0300

    ref arcoseno

--- scripts/anova-area-queimada.R
+++ scripts/anova-area-queimada.R
@@ -19,6 +19,7 @@ boxplot(percentagem~parque, data=parques_queima)
 boxplot(percentagem~tipo, data=parques_queima)
 boxplot(percentagem~local, data=parques_queima)
 # add arcsine transform  variable n quad queimado
+#https://www.statology.org/arcsine-transformation-in-r/#:~:text=An%20arcsine%20transformation%20can%20be,dealing%20with%20proportions%20and%20percentages.&text=The%20following%20examples%20show%20how%20to%20use%20this%20syntax%20in%20practice.
 parqueim <- nome_int_no_missing_anova_area_queimada_parque_enso
 parqueim %>% 
   mutate(arcsenquei = asin(sqrt(propqueim)))
